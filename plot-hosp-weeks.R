a9808eca4234715e39785b0f354f5a39044fbc02
jrgant <jason.gantenberg@gmail.com>
2019-07-19 22:04:48 -0400

    move CDC severity webscraping to its own file

--- plot-hosp-weeks.R
+++ plot-hosp-weeks.R
@@ -7,29 +7,9 @@
 pacman::p_load(flumodelr, magrittr, stringr, viridis, ggthemes, broom, rvest,
                gridExtra)
 
-# Scrape CDC Severity -------------------------------------------------------
-
-# Method described in:
-#
-# Biggerstaff M, Kniss K, Jernigan DB, Brammer L, Bresee J, Garg S, et al. 
-# Systematic Assessment of Multiple Routine and Near Real-Time Indicators to 
-# Classify the Severity of Influenza Seasons and Pandemics in the United States, 
-# 2003-2004 Through 2015-2016. Am J Epidemiol 2018;187:1040–50.
-# doi:10.1093/aje/kwx334.
-
-url <- "https://www.cdc.gov/flu/about/classifies-flu-severity.htm"
-
-cdcsv <- url %>%
-  read_html() %>%
-  html_table() %>%
-  as.data.frame(.)
-
-names(cdcsv) <- tolower(names(cdcsv))
-
-saveRDS(cdcsv, paste0("cdcsv_", Sys.Date(), ".Rds"))
-
 # Load Data -----------------------------------------------------------------
 
+cdcsv    <- readRDS("cdcsv_2019-07-19.Rds")
 hsp_file <- "hospdat/Weekly_Data_Counts.csv"
 hsp_names <- c("seas", "epiweek", "inf.a", "inf.b", "inf.ab", "inf.unk")
 
