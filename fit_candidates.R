b0adf76e5015831f103b30b54114210bcb8681a4
jrgant <jason.gantenberg@gmail.com>
2019-09-25 20:46:56 -0400

    remove Serfling section

--- fit_candidates.R
+++ fit_candidates.R
@@ -42,45 +42,6 @@ ggplot(chains$chain4, aes(x = week_order, y = prediction)) +
   scale_x_continuous("Week", labels = rep(1:31, 6)) +
   theme_clean(base_size = 15)
 
-# Serfling Model -------------------------------------------------------------
-
-# %%
-fd <- fludta %>%
-  filter(year >= 2000)
-
-fd <- fd %>%
-  mutate(
-    theta = 2 * week / 52,
-    sin_2 = sinpi(theta),
-    cos_2 = cospi(theta),
-    noninf = rnorm(nrow(.), 0, 5)
-  )
-print(fd)
-
-# %%
-fitlm1 <- lm(fludeaths ~ week + sin_2 + cos_2, data = fd)
-summary(fitlm1)
-
-
-library(MASS)
-step1 <- stepAIC(fitlm1, trace = TRUE)
-
-pred1 <- predict(fitlm1, newdata = fd)
-fd$yhat1 <- pred1
-
-setDT(fd)
-print(fd)
-
-# %%
-ggplot(fd, aes(x = week_in_order)) +
-  geom_line(aes(y = fludeaths), color = "red") +
-  geom_line(aes(y = yhat1)) +
-  geom_line(aes(y = yhat2), linetype = "dashed")
-
-
-# Modified Serfling Model ----------------------------------------------------
-
-## flumodelr::flum
 
 # Prophet Model --------------------------------------------------------------
 
