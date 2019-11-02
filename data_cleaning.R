1b7f05ebfa8b06537913abd9bac0e1270af62130
jrgant <jason.gantenberg@gmail.com>
2019-11-02 18:51:21 -0400

    pull weather station IDs

--- data_cleaning.R
+++ data_cleaning.R
@@ -16,7 +16,7 @@ pacman::p_load(
 
 # %% CDC Flu Season Severity -------------------------------------------------
 
-# Method described in:
+# @CITATION:
 
 # Biggerstaff M et al. Systematic Assessment of Multiple Routine and Near
 # Real-Time Indicators to Classify the Severity of Influenza Seasons and
@@ -129,7 +129,8 @@ whsp_rt_cols <- c(
 )
 
 # NOTE 2019-08-21:
-#   A warning is thrown when fread() gets to the CDC disclaimer text contained #   in the csv file. Benign.
+#   A warning is thrown when fread() gets to the CDC disclaimer text contained 
+#   in the csv file. Benign.
 whsp_rt <- fread(hsp_rates, col.names = whsp_rt_cols, quote = "") %>%
   # drop age-specific rates and two variables
   .[agecat == "Overall", -c("catchment", "network")] %>%
@@ -234,7 +235,7 @@ ggplot(ilisum, aes(x = weekint)) +
 
 # %% Holiday Epiweeks ---------------------------------------------------------
 
-# Sources:
+# @CITATION:
 
 # Brooks LC, Farrow DC, Hyun S, Tibshirani RJ, Rosenfeld R. Flexible Modeling
 # of Epidemics with an Empirical Bayes Framework. PLoS Comput Biol
@@ -272,6 +273,197 @@ tg_dates
 
 tg_epiweeks <- range(lubridate::epiweek(tg_dates))
 tg_epiweeks
+
+# %% Temperature Data ----------------------------------------------------------
+
+# @CITATION
+# Variable glossaries from NOAA:
+# https://www.ncdc.noaa.gov/cdo-web/webservices/v2
+
+# ... States of interest
+
+# eip = Emerging Infections Program
+eip_states <- c(
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Georgia",
+  "Maryland",
+  "Minnesota",
+  "New Mexico",
+  "New York",
+  "Oregon",
+  "Tennessee"
+)
+
+oth_states <- c("Michigan", "Ohio", "Utah")
+
+all_states <- c(eip_states, oth_states)
+print(all_states)
+
+
+# ... Pull FluView states FIPS codes
+
+# @NOTE: To pull data from NOAA's website, the user must request a token
+#        from them and store it on their computer. The rnoaa package links to
+#        the relevant instructions.
+
+fips <- rnoaa::fipscodes
+setDT(fips)
+print(fips)
+
+fipsdat <- as.data.table(
+  ncdc_locs(locationcategoryid = "ST", limit = 52)$data
+)
+fipsdat
+
+# State FIPS codes
+fv_stfips <- fipsdat[name %in% all_states, c("name", "id")]
+fv_stfips
+
+
+
+# %% Function to pull station IDs in states represented in FLuView
+# statefips = state's FIPS code
+# stdt = start date
+# enddt = end date
+# limreq = limit request, defaults to maximum limit (1000)
+
+get_stn_ids <- function(statefips,
+                        stdt = "2003-10-01",
+                        enddt = "2019-04-30",
+                        limreq = 1000) {
+
+    stndat <- ncdc_stations(locationid = statefips,
+                            startdate = stdt,
+                            enddate = enddt,
+                            limit = limreq)
+
+    totalrows <- stndat$meta$totalCount
+    
+    datlist <- list(
+      as.data.table(stndat$data)
+      )
+
+    if (totalrows < limreq | limreq < 1000) {
+      
+      findat <- datlist[[1]][, .(stnid = id, name, mindate, maxdate)]
+      findat[, stfips := statefips]
+
+      list(
+        rows_reported = totalrows,
+        rows_in_data = nrow(findat),
+        dat = findat,
+        discrepancy = totalrows - nrow(findat)
+      )
+
+    } else {
+
+      extra <- ceiling((totalrows - limreq) / limreq)
+      pass_offset <- limreq + 1
+
+      for (i in 1:extra) {
+        
+        stndatcurr <- ncdc_stations(
+          locationid = statefips,
+          limit = limreq,
+          startdate = stdt,
+          enddate = enddt,
+          offset = pass_offset)
+
+        datlist[[i + 1]] <- as.data.table(stndatcurr$data)
+        pass_offset <- pass_offset + limreq
+
+      }
+
+      findat <- rbindlist(datlist)[, .(stnid = id, name, mindate, maxdate)]
+      findat[, stfips := statefips]
+      
+      list(
+        rows_reported = totalrows,
+        rows_in_data = nrow(findat),
+        dat = findat,
+        discrepancy = totalrows - nrow(findat)
+      )
+    }
+}
+
+stations <- lapply(
+  setNames(fv_stfips$id, fv_stfips$id), 
+  function(x) get_stn_ids(statefips = x)
+  )
+
+# @DEV 2019-11-02:
+#  - If time, put all these checks into a test folder
+
+# check that stations from all states were pulled
+unique(names(stations)) == unique(fv_stfips$id)
+length(stations) == nrow(fv_stfips)
+
+# check that there were no discrepancies in requested rows vs. returned
+sapply(stations, function(x) x$rows_reported == x$rows_in_data)
+
+# check that all states have the same columns
+lapply(stations, function(x) names(x$dat))
+
+stns <- lapply(stations, function(x) x$dat) %>% rbindlist
+stns
+
+# 33 duplicate station ids detected (72 rows)
+length(unique(stns$stnid))
+dups <- stns[, .N, .(stnid)][N > 1, .(stnid)]
+dups
+
+stns[stnid %in% dups$stnid][order(stnid)]   # grab rows for all dups
+stns[grepl("NEXRAD", stnid)]                # view all NEXRAD stations
+
+# @CITATION
+# About the GHCDN: https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt
+
+# @CONUNDRUM 2019-11-02
+
+# @DESCRIPTION
+# Search for state FIPS seems to return the stations that serve a given area.
+# Example: FIPS:09 and FIPS:36 are CT and NY, respectively. Both return KBOX,
+#          which is a NEXRAD station near Boston.
+
+# @DECISION
+# Keep these records. Presumably, they reflect the weather in areas of states
+# included in the FluView data, even if the radards themselves are located in a
+# neighboring state.
+
+
+
+# %% Humidity Data ------------------------------------------------------------
+
+# Absolute humidity - Weight of water vapor in air
+
+# Relative humidity - Percentage of saturation point achieved for given air 
+#                     temperature (where saturation = 100% water vapor 
+#                     concentration)
+
+# @CITATION
+
+# Definition source:
+
+# Spellman FR, editor. Precipitation and evapotranspiration. The Handbook of
+# Meteorology, Lanham, MD: Scarecrow Press; 2013, p. 58–67.
+
+
+# @CITATION
+
+# Data source:
+
+# National Climatic Data Center/NESDIS/NOAA/U.S. Department of Commerce,
+# National Weather Service/NOAA/U.S. Department of Commerce, 557th Weather Wing/
+# U.S. Air Force/U. S. Department of Defense, and Federal Aviation Agency/U.S. 
+# Department of Transportation, 1987: NCDC TD3210 U.S. First Order Summary of
+# Day. Research Data Archive at the National Center for Atmospheric Research,
+# Computational and Information Systems Laboratory, Boulder, CO. [Available
+# online at https://doi.org/10.5065/H0J8-V485.]
+
+
+
 
 # %% Save Empirical Data ------------------------------------------------------
 
