bb901568fe180e46402c4f6f4f928d00271ba438
jrgant <jason.gantenberg@gmail.com>
2020-03-17 17:09:37 -0400

    exploratory scatterplots

---
+++ inst/04_eda.R
@@ -0,0 +1,60 @@
+# %% LOAD ----------------------------------------------------------------------
+
+library(FluHospPrediction)
+pacman::p_load(ggthemes)
+
+datl <- fread(here::here("data", "cleaned", "analysis_dataset_long.csv"))
+datw <- fread(here::here("data", "cleaned", "analysis_dataset_wide.csv"))
+
+head(datl)
+head(datw)
+
+# %% SCATTERPLOTS --------------------------------------------------------------
+
+theme_set(theme_base())
+
+# Predictor: hospitalization rate
+hosprate_pkrate <- ggplot(
+  datl, aes(x = hosprate_100k, y = pkrate)
+  ) +
+  geom_hex(bins = 70) +
+  facet_wrap(~ weekint) +
+  scale_fill_viridis_c()
+
+hosprate_pkweek <- ggplot(
+  datl, aes(x = hosprate_100k, y = pkweek)
+  ) +
+  geom_hex(bins = 70) +
+  facet_wrap(~ weekint) +
+  scale_fill_viridis_c()
+
+hosprate_cumhosp <- ggplot(
+  datl, aes(x = hosprate_100k, y = cumhosp)
+  ) +
+  geom_hex(bins = 70) +
+  facet_wrap(~ weekint) +
+  scale_fill_viridis_c()
+
+hosprate_pkrate
+hosprate_pkweek
+hosprate_cumhosp
+
+# Predictor: cumulative hospitalization rate
+cumhosp_pkrate <- ggplot(datl, aes(x = cumhosp_100k, y = pkrate)) +
+  geom_hex(bins = 70) +
+  facet_wrap(~ weekint) +
+  scale_fill_viridis_c()
+
+cumhosp_pkweek <- ggplot(datl, aes(x = cumhosp_100k, y = pkweek)) +
+  geom_hex(bins = 70) +
+  facet_wrap(~ weekint) +
+  scale_fill_viridis_c()
+
+cumhosp_cumhosp <- ggplot(datl, aes(x = cumhosp_100k, y = cumhosp)) +
+  geom_hex(bins = 70) +
+  facet_wrap(~ weekint) +
+  scale_fill_viridis_c()
+
+cumhosp_pkrate
+cumhosp_pkweek
+cumhosp_cumhosp
