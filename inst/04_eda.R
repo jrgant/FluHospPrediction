47a61d9a16191c44b617b4e8647193991494907a
jrgant <jason.gantenberg@gmail.com>
2020-04-08 21:08:48 -0400

    compare simulated curves by season template

--- inst/04_eda.R
+++ inst/04_eda.R
@@ -1,7 +1,12 @@
 # %% LOAD ----------------------------------------------------------------------
 
 suppressMessages(library(FluHospPrediction))
-pacman::p_load(ggthemes)
+
+pacman::p_load(
+  ggthemes,
+  ggridges,
+  cowplot
+)
 
 datl <- fread(here::here("data", "cleaned", "sim_dataset_long.csv"))
 datw <- fread(here::here("data", "cleaned", "sim_dataset_wide.csv"))
@@ -58,3 +63,35 @@ cumhosp_cumhosp <- ggplot(datl, aes(x = cumhosp_100k, y = cumhosp)) +
 cumhosp_pkrate
 cumhosp_pkweek
 cumhosp_cumhosp
+
+
+# %% COMPARE SIMULATED SEASONS -------------------------------------------------
+
+theme_tweak <- theme(
+  panel.border = element_blank(),
+  panel.background = element_blank(),
+)
+
+pkrates <- ggplot(datl, aes(x = pkrate, y = template)) +
+  geom_boxplot(alpha = 0)
+
+pkrates
+
+pkrate_hist <- ggplot(datl, aes(x = pkrate)) +
+  geom_histogram(color = "white", fill = "lightgray") +
+  facet_wrap(~ template)
+
+pkrate_hist
+
+pkweeks <- ggplot(datl, aes(x = pkweek, y = template)) +
+  geom_boxplot(alpha = 0) +
+  coord_flip()
+
+pkweeks
+
+curveplot <- ggplot(datl, aes(x = weekint, y = hosprate_100k)) +
+  geom_line(aes(group = cid, color = template), alpha = 0.3) +
+  guides(color = "none") +
+  facet_wrap(~template, ncol = 3)
+
+curveplot
