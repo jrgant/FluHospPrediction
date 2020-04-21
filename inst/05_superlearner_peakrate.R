fc896dbb090af30c57330ebe6563e7016bd8aa94
jrgant <jason.gantenberg@gmail.com>
2020-04-21 13:49:09 -0400

    add helper function to test make_tasks

--- inst/05_superlearner_peakrate.R
+++ inst/05_superlearner_peakrate.R
@@ -7,24 +7,35 @@ suppressMessages(library(FluHospPrediction))
 
 # ignore leave-one-out CV warning: specification intended due to- clustering
 
-current_week <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+get_week <- function(w = NULL, slurm = TRUE) {
+  
+  if (is.null(w) & slurm) {
+    as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+  } else {
+    w
+  }
+}
+
+current_week <- get_week(4)
 
 # pull the sl3 task for the current week
 pkrate_task <- suppressWarnings(
-  fhp_make_task
-  ("pkrate", current_week = current_week
+  fhp_make_task(
+    "pkrate",
+    current_week = current_week
   )
 )
 
 # specify learners and send to global environment
 cat("\n\nLearners in Stack\n")
-fhp_spec_learners(verbose = TRUE)
+fhp_spec_learners(learner_pat = "glm|lasso|mean", verbose = TRUE)
 
 # run the super learner algorithm
 fhp_run_sl(
   pkrate_task,
-  write = TRUE,
-  current_week = current_week
+  write = FALSE,
+  current_week = current_week,
+  loss_fun = loss_squared_error
 )
 
 devtools::session_info()
