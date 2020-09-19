87a418ff50287f1a200d02df0d94acc57151dc91
jrgant <jason.gantenberg@gmail.com>
2020-09-18 22:20:29 -0400

    add plots for squared error sensitivity

--- inst/06_tables-figures.R
+++ inst/06_tables-figures.R
@@ -578,7 +578,6 @@ riskdist_cumhosp
 fwrite(riskdist_cumhosp, nicefile(tabslug, "Risk-Week_Cum-Hosp", "csv"))
 
 
-
 ################################################################################
 ## SETUP: INSPECT WEIGHT ASSIGNMENTS ##
 ################################################################################
@@ -2049,28 +2048,330 @@ fwrite(
 
 
 ################################################################################
+## COMBINED ENSEMBLE SUMMARY PLOT: SQUARED ERROR LOSS ##
+################################################################################
+
+## Bind component learner data.
+pep_sqe_weights <- rbindlist(
+  idcol = "target",
+  list(
+    peakrate = pep_pr_sqe_out$data,
+    peakweek = pep_pw_sqe_out$data,
+    cumhosp = pep_ch_sqe_out$data
+  )
+)
+
+pep_sqe_weights
+
+## Bind ensemble risk data and calculate 95% confidence limits.
+pep_sqe_ens <- rbindlist(
+  idcol = "target",
+  list(
+    peakrate = rbindlist(sl_pkrate_risktables_sqe),
+    peakweek = rbindlist(sl_pkweek_risktables_sqe),
+    cumhosp = rbindlist(sl_cumhosp_risktables_sqe)
+  )
+)[learner == "SuperLearner"]
+
+pep_sqe_ens[, ":="(
+  ll95 = mean_risk - qnorm(0.975) * SE_risk,
+  ul95 = mean_risk + qnorm(0.975) * SE_risk
+)]
+
+pep_sqe_ens
+
+## Bind together the risks of the naive predictions
+pep_sqe_naive <- data.table(
+  target = c("peakrate", "peakweek", "cumhosp"),
+  naiverisk = c(pr_sqe_mnrisk, pw_sqe_mnrisk, ch_sqe_mnrisk)
+)
+
+pep_sqe_weights[, target := factor(target, levels = forder, ordered = TRUE)]
+pep_sqe_ens[, target := factor(target, levels = forder, ordered = TRUE)]
+pep_sqe_naive[, target := factor(target, levels = forder, ordered = TRUE)]
+
+pep_sqe_all <- pep_sqe_weights %>%
+  ggplot(aes(x = Week, y = log(mean_risk))) +
+  geom_point(
+    aes(size = weight, color = "Component"),
+    shape = 21
+  ) +
+  geom_pointrange(
+    data = pep_main_ens,
+    aes(x = Week, y = log(mean_risk),
+        ymin = log(ll95), ymax = log(ul95),
+        color = "Ensemble"
+        ),
+    size = 0.3,
+    shape = 21,
+    fill = "white"
+  ) +
+  geom_hline(
+    data = pep_sqe_naive,
+    aes(
+      yintercept = naiverisk,
+      color = "Mean predictor"
+    ),
+    linetype = "dashed"
+  ) +
+  facet_wrap(
+    ~ target,
+    ncol = 1,
+    scales = "free_y",
+    labeller = labeller(target = facetlabs)
+  ) +
+  scale_color_manual(
+    name = "Prediction source",
+    values = c("#dddddd", "#990000", "black")
+  ) +
+  scale_x_discrete(breaks = week_breaks) +
+  scale_size(name = "Weight") +
+  guides(linetype = FALSE) +
+  theme_base(base_family = global_plot_font) +
+  theme(
+    strip.text = element_text(face = "bold"),
+    axis.text.x = element_text(size = 8)
+  )
+
+pep_sqe_all
+
+ggsave(
+  nicefile(figslug, "Ensemble-Summary_All-Targets-SQE", "png"),
+  pep_sqe_all,
+  width = 6,
+  height = 10,
+  unit = "in",
+  device = "png"
+)
+
+
+################################################################################
 ## SENSITIVITY ANALYSIS: COMBINED FIGURE ##
 ################################################################################
 
-prsens_rwsum <- rbindlist(
+## Main analysis objects did not have a suffix.
+analysis <- c("", "_1se", "_erf")
+
+sens_compare <- lapply(setNames(analysis, analysis), function(x) {
+
+  rwsum <- rbindlist(
+    idcol = "target",
+    list(
+      pkrate = get(paste0("pkrate_rwsum", x)),
+      pkweek = get(paste0("pkweek_rwsum", x)),
+      cumhosp = get(paste0("cumhosp_rwsum", x))
+    )
+  )
+
+  risktab <- rbindlist(
+    idcol = "target",
+    list(
+      pkrate = rbindlist(get(paste0("sl_pkrate_risktables", x))),
+      pkweek = rbindlist(get(paste0("sl_pkweek_risktables", x))),
+      cumhosp = rbindlist(get(paste0("sl_cumhosp_risktables", x)))
+    )
+  )
+
+  list(rwsum = rwsum, risktab = risktab[learner == "SuperLearner"])
+})
+
+names(sens_compare) <- c("main", "lse", "erf")
+sens_compare
+
+## Bind up component learner weight summaries.
+sens_rwsum <- rbindlist(
+  idcol = "analysis",
   list(
-    main = pkrate_rwsum,
-    l1se = pkrate_rwsum_1se,
-    erf = pkrate_rwsum_erf
-  ),
-  idcol = "analysis"
+    main = sens_compare$main$rwsum,
+    lse = sens_compare$lse$rwsum,
+    erf = sens_compare$erf$rwsum
+  )
 )
 
-prsens_rwsum
-
-prsens_risktabs <- rbindlist(
+## Bind up risk tables.
+sens_risktab <- rbindlist(
+  idcol = "analysis",
   list(
-    main = rbindlist(sl_pkrate_risktables),
-    l1se = rbindlist(sl_pkrate_risktables_1se),
-    erf = rbindlist(sl_pkrate_risktables_erf)
-  ),
-  idcol = "analysis"
+    main = sens_compare$main$risktab,
+    lse = sens_compare$lse$risktab,
+    erf = sens_compare$erf$risktab
+  )
 )
 
-prsens_risktabs <- prsens_risktabs[learner != "SuperLearner"]
-prsens_risktabs
+sens_risktab[, ":="(
+  ll95 = mean_risk - qnorm(0.975) * SE_risk,
+  ul95 = mean_risk + qnorm(0.975) * SE_risk
+)]
+
+## Match up naive (median predictor) risk estimates.
+sens_naive <- data.table(
+  analysis = c(rep("main", 3), rep("lse", 3), rep("erf", 3)),
+  target = rep(c("pkrate", "pkweek", "cumhosp"), 3),
+  naiverisk = c(
+    pr_medrisk, pw_medrisk, ch_medrisk,
+    pr_1se_medrisk, pw_1se_medrisk, ch_1se_medrisk,
+    pr_medrisk, pw_medrisk, ch_medrisk
+  )
+)
+
+## Make nice facet labels.
+anlys_labs <- c(
+  main = "Main analysis",
+  lse = "Alternate trend filter penalty",
+  erf = "Component learner subset"
+)
+
+targ_labs <- c(
+  pkrate = "Peak rate",
+  pkweek = "Peak week",
+  cumhosp = "Cumulative hospitalizations"
+)
+
+## Control display order for prediction targets and analysis types.
+forder2 <- c("pkrate", "pkweek", "cumhosp")
+sens_rwsum[, target := factor(target, levels = forder2, ordered = TRUE)]
+sens_risktab[, target := factor(target, levels = forder2, ordered = TRUE)]
+sens_naive[, target := factor(target, levels = forder2, ordered = TRUE)]
+
+forder3 <- c("main", "lse", "erf")
+sens_rwsum[, analysis := factor(analysis, levels = forder3, ordered = TRUE)]
+sens_risktab[, analysis := factor(analysis, levels = forder3, ordered = TRUE)]
+sens_naive[, analysis := factor(analysis, levels = forder3, ordered = TRUE)]
+
+## Select x-axis tick mark labels to hide
+hide_xticks <- c(paste0("0", c(2:4, 6:9)), 11:14, 16:19, 21:24, 26:29)
+invislab <- rep("", length(hide_xticks))
+names(invislab) <- hide_xticks
+
+## Plot analysis comparisons.
+pep_sens_compare <- ggplot(sens_rwsum) +
+  geom_point(
+    aes(
+      x = Week, y = log(mean_risk),
+      size = weight, color = "Component learner"
+    ),
+    shape = 21
+  ) +
+  geom_segment(
+    ## This geom_segment adds an arrow where log(ll95) is undefined because the
+    ## ll95 on the original scale was a negative number.
+    data = sens_risktab[target == "pkweek" & Week == "20" & analysis == "erf"],
+    aes(
+      x = as.numeric(Week), xend = as.numeric(Week),
+      y = log(ul95), yend = -3.308303,
+      color = "Ensemble"
+    ),
+    lineend = "butt",
+    linejoin = "mitre",
+    size = 0.3,
+    arrow = arrow(length = unit(0.02, "npc"))
+  ) +
+  geom_pointrange(
+    data = sens_risktab,
+    aes(
+      x = as.numeric(Week),
+      y = log(mean_risk),
+      ymin = log(ll95),
+      ymax = log(ul95),
+      color = "Ensemble"
+    ),
+    shape = 21,
+    size = 0.3,
+    fill = "white"
+  ) +
+  geom_hline(
+    data = sens_naive,
+    aes(
+      yintercept = naiverisk,
+      color = "Median predictor"
+    ),
+    linetype = "dashed"
+  ) +
+  facet_grid(
+    target ~ analysis,
+    scales = "free_y",
+    switch = "y",
+    labeller = labeller(
+      target = targ_labs,
+      analysis = anlys_labs
+    )
+  ) +
+  scale_color_manual(
+    name = "Prediction",
+    values = c("#dddddd", "#990000", "black")
+  ) +
+  scale_size_continuous(
+    name = "Weight",
+    labels = c("> 0.00", "0.25", "0.50", "0.75", "1.00")
+  ) +
+  scale_x_discrete(breaks = week_breaks) +
+  xlab("Week") +
+  theme_base(base_family = global_plot_font) +
+  theme(
+    strip.text = element_text(face = "bold", size = 10),
+    axis.text.x = element_text(size = 8)
+  )
+
+pep_sens_compare
+
+ggsave(
+  nicefile(figslug, "Ensemble-Summary_All-Targets_Main-1SE-ERF", "png"),
+  pep_sens_compare,
+  width = 10,
+  height = 8,
+  unit = "in",
+  device = "png"
+)
+
+
+################################################################################
+## DIFFERENCE PLOTS ACROSS SENSITIVITY ANALYSIS ##
+################################################################################
+
+sens_normscale <- sens_risktab %>%
+  ggplot(
+    aes(x = Week, y = mean_risk, group = analysis)
+  ) +
+  geom_hline(
+    data = sens_naive,
+    aes(yintercept = exp(naiverisk), color = "Median predictor"),
+    linetype = "dashed"
+  ) +
+  geom_linerange(
+    aes(ymin = ll95, ymax = ul95, color = "Ensemble (95% CL)"),
+    alpha = 0.3
+  ) +
+  geom_point(
+    aes(color = "Ensemble (95% CL)"),
+    fill = "white",
+    shape = 21
+  ) +
+  facet_grid(
+    target ~ analysis,
+    scales = "free_y",
+    switch = "y",
+    labeller = labeller(
+      target = targ_labs,
+      analysis = anlys_labs
+    )
+  ) +
+  ylab("Mean risk") +
+  xlab("Week") +
+  scale_color_manual(name = "Prediction", values = c("#990000", "black")) +
+  scale_x_discrete(breaks = week_breaks) +
+  theme_base(base_family = global_plot_font) +
+  theme(
+    strip.text = element_text(face = "bold", size = 11),
+    axis.text.x = element_text(size = 8)
+  )
+
+sens_normscale
+
+ggsave(
+  nicefile(figslug, "Ensemble-Summary_All-Targets_Regular-Scale", "png"),
+  sens_normscale,
+  width = 11,
+  height = 8,
+  unit = "in",
+  device = "png"
+)
