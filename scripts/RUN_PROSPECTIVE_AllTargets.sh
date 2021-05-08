a0ec5c3ec7e863a925d2a7522674ad56c361db20
jrgant <jason.gantenberg@gmail.com>
2021-05-08 10:32:13 -0400

    sed in the correct R script for prospective analysis

--- scripts/RUN_PROSPECTIVE_AllTargets.sh
+++ scripts/RUN_PROSPECTIVE_AllTargets.sh
@@ -4,16 +4,16 @@
 # submit jobs if there are random failures on SLURM
 
 # peak rate
-sed 's/01-30/5,10,15,20,25/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2016-17'
-sed 's/01-30/5,10,15,20,25/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2017-18'
-sed 's/01-30/5,10,15,20,25/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2018-19'
+sed -e 's/01-30/5,10,15,20,25/' -e 's/04_run_superlearner.R/07_run_sl_prospective.R/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2016-17'
+sed -e 's/01-30/5,10,15,20,25/' -e 's/04_run_superlearner.R/07_run_sl_prospective.R/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2017-18'
+sed -e 's/01-30/5,10,15,20,25/' -e 's/04_run_superlearner.R/07_run_sl_prospective.R/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2018-19'
 
 # peak week
-sed 's/01-30/5,10,15,20,25/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2016-17'
-sed 's/01-30/5,10,15,20,25/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2017-18'
-sed 's/01-30/5,10,15,20,25/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2018-19'
+sed -e 's/01-30/5,10,15,20,25/' -e 's/04_run_superlearner.R/07_run_sl_prospective.R/' RUN_TARGET-pkweek_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2016-17'
+sed -e 's/01-30/5,10,15,20,25/' -e 's/04_run_superlearner.R/07_run_sl_prospective.R/' RUN_TARGET-pkweek_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2017-18'
+sed -e 's/01-30/5,10,15,20,25/' -e 's/04_run_superlearner.R/07_run_sl_prospective.R/' RUN_TARGET-pkweek_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2018-19'
 
 # cumulative hospitalizations
-sed 's/01-30/5,10,15,20,25/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2016-17'
-sed 's/01-30/5,10,15,20,25/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2017-18'
-sed 's/01-30/5,10,15,20,25/' RUN_TARGET-pkrate_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2018-19'
+sed -e 's/01-30/5,10,15,20,25/' -e 's/04_run_superlearner.R/07_run_sl_prospective.R/' RUN_TARGET-cumhosp_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2016-17'
+sed -e 's/01-30/5,10,15,20,25/' -e 's/04_run_superlearner.R/07_run_sl_prospective.R/' RUN_TARGET-cumhosp_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2017-18'
+sed -e 's/01-30/5,10,15,20,25/' -e 's/04_run_superlearner.R/07_run_sl_prospective.R/' RUN_TARGET-cumhosp_LAMBDA-lambda-min.sh | sbatch --export=ALL,OBS_SEASON='2018-19'
